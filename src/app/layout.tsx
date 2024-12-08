55394259e5033274033dc80e918d7b63b9ae936c
xiemenwaidao <menxielijiao@gmail.com>
2024-12-08 20:30:31 +0900

    🍱

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,18 @@ export const metadata: Metadata = {
   title: '集讚館｜好吧我聽你的',
   description:
     'Xでいいねした投稿を後で見返すためのサイトです。いいねいいねそれいいね。',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon/favicon.ico' },
+    ],
+    apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180' }],
+  },
+  manifest: '/favicon/site.webmanifest',
+  appleWebApp: {
+    title: '集讚館｜好吧我聽你的',
+  },
 };
 
 interface DateInfo {
