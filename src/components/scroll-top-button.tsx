3604d3c28cbdfebefbcd755b69fb76654b9c790e
xiemenwaidao <menxielijiao@gmail.com>
2025-05-05 17:17:42 +0900

    :adhesive_bandage: update: スクロールトップボタン修正

--- src/components/scroll-top-button.tsx
+++ src/components/scroll-top-button.tsx
@@ -4,20 +4,18 @@ import { Button } from '@/components/ui/button';
 
 export const ScrollTopButton = () => {
   return (
-    <div className="">
-      <Button
-        variant={'ghost'}
-        size="icon"
-        onClick={() => {
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-          });
-        }}
-        className="fixed bottom-4 right-4 z-50 backdrop-blur-md bg-gray-900/50 border-b border-gray-700/30 rounded-full p-3 transition-all duration-200"
-      >
-        ☝️
-      </Button>
-    </div>
+    <Button
+      variant={'ghost'}
+      size="icon"
+      onClick={() => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }}
+      className="fixed bottom-5 right-5 md:right-[calc(50vw-14rem+1.25rem)] backdrop-blur-md bg-gray-900/50 border-b border-gray-700/30 rounded-full p-3 transition-all duration-200 z-50"
+    >
+      ☝️
+    </Button>
   );
 };
